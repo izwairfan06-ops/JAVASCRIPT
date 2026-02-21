17a9c12f39024e6180cd858bc40fe8300cb7e0ec
izwairfan06-ops <izwairfan06@gmail.com>
2026-02-21 11:51:49 +0500

    1_variables.js

---
+++ 1_variables.js
@@ -0,0 +1,7 @@
+const accountId=1444553 //const not changed
+let accountEmail= "jjj@google.com"
+var accountPasswaord="1234"
+accountCity="faisalabad"
+let accountstate
+console.log(accountId);
+console.table([accountId,accountEmail,accountPasswaord,accountCity,accountstate]);
