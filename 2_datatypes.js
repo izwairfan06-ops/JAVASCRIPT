7a7b30477e9af65123add627fac9d8beffcc8435
izwairfan06-ops <izwairfan06@gmail.com>
2026-02-21 12:05:59 +0500

    2_datatypes.js

---
+++ 2_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as newer version
+//  alert("hello");  ---we are using nodejs, not browser
+
+let name= "izwa"
+let age= 18
+let idLoggedIn= false
+let state=null
+//datatypes
+/* number 
+   bigint
+   string
+   boolean
+   null
+   undefined
+   symbol
+   object
+*/
+console.log(typeof age);
+console.log(typeof null);
